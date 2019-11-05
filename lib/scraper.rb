dc56ce280affca7cc3de3e26fc06289dc46368c3
Lauren Dove <elledoveinc@gmail.com>
2019-11-05 02:05:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,7 +7,18 @@ class Scraper
    #doc = Nokogiri::HTML(open(index_url))
     #binding.pry
     
-    
+    index_page = Nokogiri::HTML(open(index_url))
+    students = []
+    index_page.css("div.roster-cards-container").each do |card|
+      card.css(".student-card a").each do |student|
+        student_profile_link = "#{student.attr('href')}"
+        student_location = student.css('.student-location').text
+        student_name = student.css('.student-name').text
+        students << {name: student_name, location: student_location, profile_url: student_profile_link}
+      end
+    end
+    students
+  end
     
     
     
